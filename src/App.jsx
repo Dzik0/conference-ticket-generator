0cf519ce89bb68bd7445b0cc5a2ed4bd5654f932
Piotr <hunter976@gmail.com>
2025-05-20 21:22:16 +0200

    Changed some names and started codfing form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Form from './Form.jsx';
+
 function App() {
   return (
-    <div className="tablet:bg-neutral-500 grid grid-cols-2 border-1">
-      <div className="h-20 w-20 bg-red-500"></div>
-      <div className="h-20 w-20 bg-red-500"></div>
+    <div>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.jsx
@@ -0,0 +1,17 @@
+function Form() {
+  return (
+    <div className="px-6 py-10 text-center">
+      <div className="flex justify-center">
+        <img src="/logo-full.svg" alt="" />
+      </div>
+      <h1 className="mt-10 text-3xl font-semibold">
+        Your Journey to Coding Conf 2025 Starts Here!
+      </h1>
+      <p className="mt-5 text-xl text-neutral-400">
+        Secure your spot at next year's biggest coding conference.
+      </p>
+    </div>
+  );
+}
+
+export default Form;
