0d16936bd0ec320561dbf642896332f9d3ace086
Piotr <hunter976@gmail.com>
2025-05-20 19:07:42 +0200

    added styleguide

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
   return (
-    <h1 className="bg-sky-700 px-4 py-2 text-white sm:px-8 sm:py-3">
-      Hello Word!
-    </h1>
+    <div className="tablet:bg-neutral-500 grid grid-cols-2 border-1">
+      <div className="h-20 w-20 bg-red-500"></div>
+      <div className="h-20 w-20 bg-red-500"></div>
+    </div>
   );
 }
 export default App;
